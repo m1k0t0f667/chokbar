d8cba8945ff1a3ba2b75e2be2df7cc72b94d65e3
lelemathrin <leo.mathurin@epitech.digital>
2023-09-25 15:36:51 +0200

    added leobutton

---
+++ components/Buttonleo.jsx
@@ -0,0 +1,63 @@
+import { StyleSheet, View, Text } from 'react-native';
+import Vector from '../assets/QRCode.svg';
+import theme from '../theme';
+
+export function Buttonleo(props) {
+  const classes = {
+    root: [
+      styles.root,
+      props.property1 === 'Active' && styles.rootProperty1Active,
+    ],
+    scannezEtGagnez: [
+      styles.scannezEtGagnez,
+      props.property1 === 'Active' && styles.scannezEtGagnezProperty1Active,
+    ],
+  };
+
+  return (
+    <View style={classes.root}>
+      <Vector />
+      <Text style={classes.scannezEtGagnez}>
+        Scannez et gagnez
+      </Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  root: {
+    display: 'flex',
+    height: 55,
+    justifyContent: 'center',
+    alignItems: 'center',
+    flexShrink: 0,
+    backgroundColor: '#000',
+    elevation: 2,
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 2,
+    flexDirection: 'row',
+    paddingVertical: 0,
+    paddingHorizontal: 30,
+    borderRadius: 60,
+  },
+  rootProperty1Active: {
+    backgroundColor: theme.colors.principalOrange,
+  },
+  scannezEtGagnez: {
+    color: '#FFF',
+    fontFamily: 'Prompt',
+    fontSize: 27.6,
+    fontStyle: 'normal',
+    fontWeight: '400',
+    lineHeight: 27.6,
+    letterSpacing: -0.552,
+  },
+  scannezEtGagnezProperty1Active: {
+    color: '#000',
+  },
+});
