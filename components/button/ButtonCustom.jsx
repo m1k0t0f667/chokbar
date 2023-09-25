cb8aff79b8dd4ba3e4c8a41588f3d0ba72540dc6
m1k0t0f667 <h.briffaux@gmail.com>
2023-09-25 15:46:06 +0200

    Salut

--- components/button/ButtonCustom.jsx
+++ components/button/ButtonCustom.jsx
@@ -1,12 +1,31 @@
 import { Image, StyleSheet, Text, View,Button } from 'react-native'
 import React from 'react'
 import QRCode from "../../assets/QRCode.svg"
+import { useFonts } from 'expo-font';
+
 
 const ButtonCustom = () => {
+
+  const [fontsLoaded, fontError] = useFonts({
+    'Prompt-Regular': require('../../assets/fonts/Prompt/Prompt-Regular.ttf'),
+    'Inter-SemiBoldItalic': 'https://rsms.me/inter/font-files/Inter-SemiBoldItalic.otf?v=3.12',
+    
+  });
+  if (!fontsLoaded) {
+    return null;
+  }
+
+  // const onLayoutRootView = useCallback(async () => {
+  //   if (fontsLoaded || fontError) {
+  //     await SplashScreen.hideAsync();
+  //   }
+  // }, [fontsLoaded, fontError]);
+
+
   return (
     <View style={styles.container}>
         <QRCode height={20} width={20}/>
-        <Text style={styles.texte}>Buttohjdhzdn</Text>
+        <Text style={[styles.texte,{fontFamily:"Inter-SemiBoldItalic"}]}>Scannez et gagnez</Text>
     
     </View>
   )
@@ -20,14 +39,16 @@ const styles = StyleSheet.create({
         flexDirection:"row",
         justifyContent:"center",
         backgroundColor:"black",
-        width:"30",
         height:55,
         alignItems:"center",
-        
-       
+        gap: "5px",
+        paddingLeft: 20, 
+        paddingRight: 20,
+       borderRadius:60
         
     },
     texte:{
-        color:"white"
+        color:"white",
+        fontFamily: 'Prompt-Regular', // Use the exact font family name
     }
 })    
