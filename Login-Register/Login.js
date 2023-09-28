fb4fce45a23bd66ea94463bffdcb24703fb7ab3b
Flavien-Walk <114407085+Flavien-Walk@users.noreply.github.com>
2023-09-28 11:26:40 +0200

    Réglage login/register position

--- Login-Register/Login.js
+++ Login-Register/Login.js
@@ -25,9 +25,10 @@ function Login() {
   };
 
   const handleForgotPassword = () => {
-    // Ici, vous pouvez ajouter une action à exécuter lorsque l'utilisateur clique sur "Mot de passe oublié ?"
+    // Ajouter une action à exécuter lorsque l'utilisateur clique sur "Mot de passe oublié ?"
   };
 
+ 
   return (
     <View style={styles.container}>
       <View style={styles.inputIconContainer}>
@@ -39,7 +40,8 @@ function Login() {
           onChangeText={setEmail}
         />
       </View>
-      <View style={[styles.inputIconContainer, { marginTop: 65 } ]}>
+      
+      <View style={[styles.inputIconContainer, { marginTop: 10 }]}>
         <Image source={lockIcon} style={styles.icon} />
         <TextInput
           style={styles.inputPassword}
@@ -52,26 +54,33 @@ function Login() {
           <Image source={hidePassword ? eyeOffIcon : eyeIcon} style={styles.icon} />
         </TouchableOpacity>
       </View>
-      <TouchableOpacity style={styles.checkboxContainer} onPress={() => setStayConnected(!stayConnected)}>
-        <View style={[styles.checkbox, stayConnected && styles.checkboxChecked]} />
-        <Text style={styles.checkboxLabel}>Resté connecté</Text>
-      </TouchableOpacity>
-      <TouchableOpacity onPress={handleForgotPassword} style={styles.forgotPasswordLink}>
-        <Text style={styles.forgotPasswordText}>Mot de passe oublié ?</Text>
-      </TouchableOpacity>
+
+      <View style={{ flexDirection: 'row', justifyContent: 'space-between', width: 342, marginTop: 8 }}>
+        <TouchableOpacity style={styles.checkboxContainer} onPress={() => setStayConnected(!stayConnected)}>
+          <View style={[styles.checkbox, stayConnected && styles.checkboxChecked]} />
+          <Text style={styles.checkboxLabel}>Resté connecté</Text>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={handleForgotPassword}>
+          <Text style={styles.forgotPasswordText}>Mot de passe oublié ?</Text>
+        </TouchableOpacity>
+      </View>
+
       <TouchableOpacity style={styles.button} onPress={login}>
         <Text style={styles.buttonText}>Se connecter</Text>
       </TouchableOpacity>
+
       <View style={styles.dividerContainer}>
         <View style={styles.divider} />
         <Text style={styles.dividerText}>ou continuer avec</Text>
         <View style={styles.divider} />
       </View>
+
       <Image source={googleIcon} style={styles.googleIcon} />
-      <Text style={styles.noAccountText}>Pas encore de compte ?</Text>
-      <Text style={styles.createAccountText}>Crée un compte</Text>
-      
-      
+
+      <View style={{ flexDirection: 'row', marginTop: 8 }}>
+        <Text style={styles.noAccountText}>Pas encore de compte ?</Text>
+        <Text style={styles.createAccountText}>  Crée un compte</Text>
+      </View>
     </View>
   );
 }
