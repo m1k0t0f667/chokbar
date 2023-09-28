9a6ee6749e3e61eaaf481199665b38552e7c6fd7
thibaud-cmbz <thibaud.combaz-deville@epitech.digital>
2023-09-28 16:42:28 +0200

    Update offre.jsx

--- components/offers/offre.jsx
+++ components/offers/offre.jsx
@@ -1,59 +1,49 @@
 import React from 'react';
-import { StyleSheet, View, Text, Image, ScrollView } from 'react-native';
+import { StyleSheet, View, Text, Image, Modal, TouchableOpacity } from 'react-native';
 import { Color, FontFamily, FontSize } from '../../GlobalStyles';
 
-
-
 export default function Offre() {
-  const bars = [
-    { name: 'Exemple de bar', offer: 'Exemple d’offre', rating: 4.1, image: require('../../assets/bar1.jpg') }, 
-    { name: 'test1', offer: 'Offre 1', rating: 4.2, image: require('../../assets/bar2.jpg') },
-    { name: 'test2', offer: 'Offre 2', rating: 4.3, image: require('../../assets/bar3.jpg') },
-    { name: 'test3', offer: 'Offre 3', rating: 4.4, image: require('../../assets/bar4.jpg') },
-    { name: 'test4', offer: 'Offre 4', rating: 4.5, image: require('../../assets/bar1.jpg') },
-    { name: 'test5', offer: 'Offre 5', rating: 4.6, image: null },
-    { name: 'test6', offer: 'Offre 6', rating: 4.7, image: null },
-    { name: 'test7', offer: 'Offre 7', rating: 4.8, image: null },
-  ];
+  const [modalVisible, setModalVisible] = React.useState(false);
 
   return (
-    <ScrollView style={{ flex: 1 }}>
-      <View style={styles.container}>
-        {bars.map((bar, index) => (
-          <View key={index} style={styles.root}>
-            <View style={styles.rectangle13} />
-            <View style={styles.rectangle10}>
-              {bar.image && <Image source={bar.image} style={{ width: '100%', height: '100%' }} />}
-            </View>
-
-            <Text style={styles.exempleDeBar}>
-              {bar.name}
-            </Text>
-
-            <View style={styles.offre}>
-              <View style={styles.frame13}>
-                <Text style={styles.exempleDoffre}>
-                  {bar.offer}
-                </Text>
-              </View>
-            </View>
-            <View style={styles.ratingCircle}>
-              <Text style={styles.ratingText}>{bar.rating}</Text>
-            </View>
-          </View>
-        ))}
+    <TouchableOpacity onPress={() => setModalVisible(true)} style={{...styles.root, ...styles.clickable}}>
+      <View style={styles.rectangle10}>
+        {/* Ajoutez votre image ici */}
+        {/* <Image source={require('path_to_your_image.png')} style={{ width: '100%', height: '100%' }} /> */}
       </View>
-    </ScrollView>
+
+      <Text style={styles.exempleDeBar}>
+        Exemple de bar
+      </Text>
+      
+      <View style={styles.offre}>
+        <View style={styles.frame13}>
+          <Text style={styles.exempleDoffre}>
+            Exemple d’offre
+          </Text>
+        </View>
+      </View>
+      
+      <View style={styles.ratingCircle}>
+        <Text style={styles.ratingText}>4.1</Text>
+      </View>
+
+      <Modal
+        animationType="slide"
+        transparent={false}
+        visible={modalVisible}
+        onRequestClose={() => setModalVisible(false)}
+      >
+        <View style={styles.modalContent}>
+          <Text>Contenu du modal pour OffreBar</Text>
+          <TouchableOpacity onPress={() => setModalVisible(false)}>
+            <Text>Fermer</Text>
+          </TouchableOpacity>
+        </View>
+      </Modal>
+    </TouchableOpacity>
   );
 }
-
-
-
-
-
-
-
-
 
 const styles = StyleSheet.create({
   root: {
@@ -61,22 +51,16 @@ const styles = StyleSheet.create({
     height: 160,
     backgroundColor: '#FDFDFD',
     borderRadius: 15,
-    elevation: 5,  // Shadow for Android
-    shadowColor: '#000', // Shadow for iOS
+    elevation: 5,
+    shadowColor: '#000',
     shadowOffset: { width: 0, height: 0 },
     shadowOpacity: 0.25,
     shadowRadius: 15,
     justifyContent: 'center',
     alignItems: 'center',
-    marginBottom: 15,
   },
-  rectangle13: {
-    position: 'absolute',
-    top: 0,
-    width: '100%',
-    height: '100%',
-    borderRadius: 15,
-    backgroundColor: '#FDFDFD',
+  clickable: {
+    padding: 10,
   },
   rectangle10: {
     position: 'absolute',
@@ -86,7 +70,6 @@ const styles = StyleSheet.create({
     borderTopLeftRadius: 15,
     borderTopRightRadius: 15,
     backgroundColor: '#E2E2E2',
-
   },
   exempleDeBar: {
     position: 'absolute',
@@ -98,22 +81,22 @@ const styles = StyleSheet.create({
   },
   offre: {
     position: 'absolute',
-    top: 25, // Ajusté pour une position plus centrale
-    left: 0, // Collé à gauche
+    top: 25,
+    left: 0,
     flexDirection: 'row',
-    width: '100%', // Pour que le bandeau s'étende sur toute la largeur si nécessaire
+    width: '100%',
   },
   frame13: {
     backgroundColor: '#FF914D',
     borderTopRightRadius: 15,
     borderBottomRightRadius: 15,
-    paddingHorizontal: 15, // Agrandir le padding horizontal pour que le bandeau soit plus gros
-    paddingVertical: 2,   // Agrandir le padding vertical pour que le bandeau soit plus gros
+    paddingHorizontal: 15,
+    paddingVertical: 2,
     alignItems: 'center',
     justifyContent: 'center',
   },
   exempleDoffre: {
-    fontSize: 12, // Ajuster la taille de la police pour qu'elle soit plus grande
+    fontSize: 12,
     color: '#FFF',
     fontFamily: FontFamily.promptRegular,    
   },
@@ -131,11 +114,9 @@ const styles = StyleSheet.create({
   ratingText: {
     fontSize: 12,
   },
-
-  container: {
-    flexDirection: 'column', 
+  modalContent: {
+    flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
   },
-  
 });
